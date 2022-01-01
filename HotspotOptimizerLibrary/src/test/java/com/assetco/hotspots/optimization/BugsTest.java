033c56ec36b79e961b4bebdd14e9eeb620c035ac
Miko Belardo <embelardo@gmail.com>
2022-01-01 12:03:50 -0500

    First commit

---
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
@@ -0,0 +1,10 @@
+package com.assetco.hotspots.optimization;
+
+import org.junit.jupiter.api.Test;
+
+public class BugsTest {
+    @Test
+    void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# hotspot-optimizer-library
+Practice project for [Unit Testing: Fix and Test Bugs \[Guernsey\]](https://bit.ly/3pzDVvo).
